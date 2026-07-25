5cb8e436c9fd72c9fb905fb21d6e34e29d0f5283
BruhImaJust9 <bruhimajustdothisname@gmail.com>
2026-07-25 19:30:25 -0400

    Fix formatting in voice input section

--- streamlit_app.py
+++ streamlit_app.py
@@ -301,9 +301,9 @@ with col_popover:
         audio_bytes = audio_recorder(...)
         text="Click to Record Voice",
         recording_color="#e84c3d",
-         neutral_color="#6aa84f",
+        neutral_color="#6aa84f",
         icon_name="microphone",
-         icon_size="2x",
+        icon_size="2x",
         key="voice_recorder"
         )
         
