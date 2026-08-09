71b19f05ff676e16243bbcb33d59af391fe211ae
BruhImaJust9 <bruhimajustdothisname@gmail.com>
2026-08-09 11:35:38 -0400

    Add new model option 'llama-3.2-11b-vision-preview'

--- streamlit_app.py
+++ streamlit_app.py
@@ -644,6 +644,7 @@ with st.sidebar:
         [
             "llama-3.3-70b-versatile",
             "llama-3.1-8b-instant",
+            "llama-3.2-11b-vision-preview",
             "mixtral-8x7b-32768",
         ],
     )
